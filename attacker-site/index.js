eac62f3fd3bdca8de3059ba9bf98f641f8213554
anerli <anders.erik.lie@gmail.com>
2021-12-16 14:57:20 -0600

    session stuff

---
+++ attacker-site/index.js
@@ -0,0 +1,3 @@
+console.log("This is the attacker site");
+
+//fetch("http://localhost:8000")
--- target-site/server.py
+++ target-site/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Cookie, Response, Body
+from fastapi.staticfiles import StaticFiles
 import secrets
 
 '''
@@ -7,27 +8,35 @@ Mock banking site.
 
 app = FastAPI()
 
+
 sessions = {}
 
 # TODO: Break
 
+api = FastAPI(root_path="/api")
 
-@app.get("/")
+app.mount("/api", api)
+app.mount("/", StaticFiles(directory='public', html=True), name="static")
+
+
+@api.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/login/{username}")
+@api.get("/login/{username}")
 async def login(username: str, response: Response):
     # https://fastapi.tiangolo.com/advanced/response-cookies/
-    token = secrets.token_bytes(16)
+    token = secrets.token_hex(16)
     response.set_cookie(key="sessioncookie", value=token)
     sessions[username] = token
     #return {"detail": "Success"}
+    print('Sessions:', sessions)
 
 def check_token(username, token):
     return username in sessions and sessions[username] == token
 
-@app.post("/transfer")
+@api.post("/transfer")
 async def transfer_funds():
     # https://fastapi.tiangolo.com/tutorial/cookie-params/
     pass
+
